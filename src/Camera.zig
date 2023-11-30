607b6ecae84789c4cb349e80aab5904fe44c8dee
Julian Vesper <jvesper95@gmail.com>
2023-12-01 00:49:22 +0100

    Move camera to own struct

---
+++ src/Camera.zig
@@ -0,0 +1,115 @@
+const std = @import("std");
+const collision = @import("collision.zig");
+
+const Interval = @import("Interval.zig");
+const Ray = @import("Ray.zig");
+const Vec3 = @import("Vec3.zig");
+
+const Color = Vec3;
+const HitRecord = collision.HitRecord;
+const Hittable = collision.Hittable;
+const Point3 = Vec3;
+
+aspect_ratio: f64,
+
+image_width: usize,
+image_height: usize,
+
+center: Point3,
+pixel_00_loc: Point3,
+
+pixel_delta_u: Vec3,
+pixel_delta_v: Vec3,
+
+const Self = @This();
+
+pub fn new(aspect_ratio: f64, image_width: usize) Self {
+    const image_height: usize = blk: {
+        const height: usize = @intFromFloat(@as(f64, @floatFromInt(image_width)) / aspect_ratio);
+
+        if (height > 1) {
+            break :blk height;
+        } else {
+            break :blk 1;
+        }
+    };
+
+    const center = Point3.default();
+
+    const focal_length: f64 = 1.0;
+    const viewport_height: f64 = 2.0;
+    const viewport_width: f64 = viewport_height * (@as(f64, @floatFromInt(image_width)) / @as(f64, @floatFromInt(image_height)));
+
+    // Calculate the vectors across the horizontal and down the vertical viewport edges
+    const viewport_u = Vec3.new(viewport_width, 0, 0);
+    const viewport_v = Vec3.new(0, -viewport_height, 0);
+
+    // Calculate the horizontal and vertical delta vectors from pixel to pixel
+    const pixel_delta_u = viewport_u.div_scalar(@floatFromInt(image_width));
+    const pixel_delta_v = viewport_v.div_scalar(@floatFromInt(image_height));
+
+    // Calculate the location of the upper left pixel.
+    const viewport_upper_left = center
+        .sub(Vec3.new(0, 0, focal_length))
+        .sub(viewport_u.mul_scalar(0.5))
+        .sub(viewport_v.mul_scalar(0.5));
+
+    const pixel_00_loc = viewport_upper_left
+        .add(pixel_delta_u.add(pixel_delta_v).mul_scalar(0.5));
+
+    return .{
+        .aspect_ratio = aspect_ratio,
+        .image_width = image_width,
+        .image_height = image_height,
+        .center = center,
+        .pixel_00_loc = pixel_00_loc,
+        .pixel_delta_u = pixel_delta_u,
+        .pixel_delta_v = pixel_delta_v,
+    };
+}
+
+pub fn render(self: Self, writer: anytype, world: Hittable) !void {
+    // Header
+    try writer.print("P3\n", .{});
+    try writer.print("{d} {d}\n", .{ self.image_width, self.image_height });
+    try writer.print("255\n", .{});
+
+    // Data
+    for (0..self.image_height) |h| {
+        std.debug.print("\rScanlines remaining: {}", .{self.image_height - h});
+
+        for (0..self.image_width) |w| {
+            const pixel_center = self.pixel_00_loc
+                .add(self.pixel_delta_u.mul_scalar(@floatFromInt(w)))
+                .add(self.pixel_delta_v.mul_scalar(@floatFromInt(h)));
+            const ray_direction = pixel_center.sub(self.center);
+
+            const ray = Ray.new(self.center, ray_direction);
+            const pixel_color = ray_color(ray, world);
+
+            try writer.print("{}\n", .{pixel_color});
+        }
+    }
+    std.debug.print("\rDone.                                                    \n", .{});
+}
+
+fn ray_color(ray: Ray, world: Hittable) Color {
+    var record = HitRecord{};
+
+    if (world.hit(ray, Interval.new(0, std.math.inf(f64)), &record)) {
+        return record.normal
+            .add(Color.new(1, 1, 1))
+            .mul_scalar(0.5);
+    }
+
+    const unit_direction = ray.direction.unit_vector();
+    const a = 0.5 * (unit_direction.y() + 1.0);
+
+    const start_color = Color.new(1, 1, 1);
+    const end_color = Color.new(0.5, 0.7, 1.0);
+
+    // Do a linear interpolation (lerp)
+    return start_color
+        .mul_scalar(1.0 - a)
+        .add(end_color.mul_scalar(a));
+}
